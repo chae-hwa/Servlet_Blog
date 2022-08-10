73f1b816644c9fe1e5a609ea05c9bdd95e5e2871
chae-hwa <ihj0519@naver.com>
2022-08-11 01:54:40 +0900

    findBySubjectLike

--- src/main/java/com/example/sbb/QuestionRepository.java
+++ src/main/java/com/example/sbb/QuestionRepository.java
@@ -2,6 +2,7 @@ package com.example.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
@@ -9,4 +10,6 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String s);
 }
