e84bb0dde61b99472e0fef29d9dbc698b1c7cce6
chae-hwa <ihj0519@naver.com>
2022-08-11 02:01:32 +0900

    삭제 메서드

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -74,4 +74,14 @@ class SbbApplicationTests {
         q.setSubject("수정된 제목");
         this.questionRepository.save(q);
     }
+
+    @Test
+    void testJpa7() {
+        assertEquals(2, this.questionRepository.count());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
+    }
 }
