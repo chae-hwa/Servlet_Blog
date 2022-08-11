33898fbbd5d88c68af282ed4bb9a387835da32ae
chae-hwa <ihj0519@naver.com>
2022-08-11 23:28:46 +0900

    question truncate TEST 추가

--- src/main/java/com/example/sbb/QuestionRepository.java
+++ src/main/java/com/example/sbb/QuestionRepository.java
@@ -1,7 +1,10 @@
 package com.example.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +15,9 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
 
     List<Question> findBySubjectLike(String s);
+
+    @Transactional
+    @Modifying
+    @Query(value = "truncate question", nativeQuery = true)
+    void truncate();
 }
