d4e2ea7bcaacd731c5e508a9b908f2751876c2b5
chae-hwa <ihj0519@naver.com>
2022-08-16 23:58:46 +0900

    답변 등록폼 작업

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
+@RequestMapping("/question")
 @RequiredArgsConstructor // 생성자 주입
 public class QuestionController {
     // @Autowired // 필드 주입
     private final QuestionService questionService;
 
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     // 이 자리에 @ResponseBody가 없으면 resources/question_list/question_list.html 파일을 뷰로 삼는다.
     public String list(Model model) {
         List<Question> questionList = questionService.getList();
@@ -27,13 +28,13 @@ public class QuestionController {
         return "question_list";
     }
 
-    @RequestMapping("/question/list2")
+    @RequestMapping("/list2")
     @ResponseBody
     public String list2() {
         return "하하111777";
     }
 
-    @RequestMapping(value = "/question/detail/{id}")
+    @RequestMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id){
         Question question = questionService.getQuestion(id);
         model.addAttribute("question",question);
