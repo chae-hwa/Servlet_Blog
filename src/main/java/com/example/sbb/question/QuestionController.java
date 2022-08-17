f81bac379867b1fa2423a2971bd8c1823c69d2da
chae-hwa <ihj0519@naver.com>
2022-08-17 23:33:59 +0900

    질문 등록 처리

--- src/main/java/com/example/sbb/question/QuestionController.java
+++ src/main/java/com/example/sbb/question/QuestionController.java
@@ -3,10 +3,7 @@ package com.example.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,5 +43,11 @@ public class QuestionController {
     @GetMapping("/create")
     public String questionCreate(){
         return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content ){
+        questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 }
