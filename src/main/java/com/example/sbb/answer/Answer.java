5d1740705992eac5cab22f70e60063bcc5719ec3
chae-hwa <ihj0519@naver.com>
2022-08-22 23:30:06 +0900

    Answer, Question 엔티티에 modifyDate 컬럼 추가

--- src/main/java/com/example/sbb/answer/Answer.java
+++ src/main/java/com/example/sbb/answer/Answer.java
@@ -20,8 +20,8 @@ public class Answer {
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    @CreatedDate
     private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
 
     @ManyToOne
     private Question question;
