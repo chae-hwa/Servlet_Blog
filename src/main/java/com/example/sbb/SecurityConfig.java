abf1b51be2ceb2737d957949de9e7d2445b95af6
chae-hwa <ihj0519@naver.com>
2022-08-22 22:27:16 +0900

    로그아웃 유저는 질문, 답변 시 로그인 페이지로 이동

--- src/main/java/com/example/sbb/SecurityConfig.java
+++ src/main/java/com/example/sbb/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.sbb;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +14,8 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
